d917704ab04469c7dedeb18d4320e08520c36ec5
pram <pramudya@gmail.com>
2024-03-19 21:28:34 +0700

    blajar variabel tipe data di js sama coach coursenet coy

---
+++ algoPseudocode.js
@@ -0,0 +1,79 @@
+/**
+ * STUDY CASE 1
+ * ALGORITMA :
+ * 1. input variabel panjang
+ * 2. input variabel lebar
+ * 3. masukan rumus kelilingPP = 2*(panjang + lebar)
+ * 4. cetak kelilingPP
+ * Tentukan algoritma dan pseudocode untuk menghitung keliling persegi panjang
+ * BEGIN
+ * INPUT panjang
+ * INPUT lebar
+ * keliling = 2*(panjang+lebar)
+ * OUTPUT "Kelilingnya adalah " + keliling
+ * END
+ *
+ *
+ * STUDY CASE 2
+ * Tentukan algoritma dan pseudocode untuk menghitung luas permukaan tabung
+ * ALGORITMA
+ * 1. Mulai
+ * 2. Masukan jari-jari dan tinggi tabung
+ * hitung luas permukaan tabung dengan rumus : L = 2 x pi x r x (r + tinggi)
+ * Tampilkan hasil luas permukaan
+ * Selesai
+ *  BEGIN
+ * INPUT jari_jari
+ * INPUT tinggi
+ * luas_permukaan_tabung = 2* 3.14 * jari_jari *(jari_jari + tinggi)
+ * OUTPUT "Luas Permukaan Tabung adalah " + luas_permukaan_tabung
+ *
+ * STUDY CASE 3
+ * Tentukan algoritma dan psedocode volume 1/4 bola
+ *
+ *
+ */
+
+/**
+ * JAWABAN COACH
+ * STUDY CASE 1
+ *
+ * ALGORITMA
+ * 1. Buat variable panjang
+ * 2. Buat variable panjang
+ * 3. Buat variable keliling
+ * 4. Masukan rumus keliling = 2 x panjang + 2 x lebar
+ * 5. Cetak keliling
+ *
+ * PSEUDOCODE
+ * STORE panjang WITH ANY NUMBER
+ * STORE lebar WITH ANY NUMBER
+ *
+ * STORE keliling WITH 2 MUL panjang ADD 2 MUL lebar
+ * DISPLAY keliling
+ *
+ * STUDY CASE 2
+ * ALGORITMA
+ * 1. Cari luas permukaan tabung = 2 x pi x r x r + 2 x pi x r x t
+ * 2. Buat variabel pi
+ * 3. Buat variabel r
+ * 4. Buat variabel t
+ * 5. Buat variabel luasPermukaan
+ * 6. Buat
+ *
+ *
+ * STUDY CASE 3
+ * 1. Cari rumus volume bola = 4/3 x pi x r x r x r
+ * 2. Buat variable pi
+ * 3. Buat variable r
+ * 4. Buat variable volume
+ * 5. Isi volume dengan rumus = 1/4 x 4/3 x pi x r x r x r
+ * 6. Cetak volume
+ *
+ * Pseudocode
+ * STORE pi WITH 3.34
+ * STORE r WITH ANY NUMBER
+ *
+ * STORE volume WITH 1/4 MUL 4/3 MUL pi  MUL r MUL r MUL r
+ * DISPLAY volume
+ */
---
+++ variableTipeData.js
@@ -0,0 +1,31 @@
+// var number = 10;
+// console.log(number);
+
+// var str = "buku";
+// console.log(str);
+
+// var isCorrect = true;
+// console.log(nisCorrect);
+
+// var a = 10;
+// var b = "5";
+// var total = a / b;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log();
+// console.log(typeof total);
+
+//OPERATOR LOGIC
+
+var x = 10;
+var y = "10";
+
+var z = x == y;
+console.log(z);
+
+console.log();
+
+var z = x === y;
+console.log(z);
+// == membandingkan value saja
+// === membandingkan value dan tipe data
